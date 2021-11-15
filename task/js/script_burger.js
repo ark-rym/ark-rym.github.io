16375eb72a7956cc6d149d49307ba8f95e1a3772
ark-rym <arymbeko@yandex.ru>
2021-11-15 10:36:43 +0300

    1.6 5th version

--- task/js/script_burger.js
+++ task/js/script_burger.js
@@ -91,22 +91,24 @@ document.addEventListener('DOMContentLoaded', () => {
     textHeiht = 160
   } 
 
-})
 
-
-var buttonTextOpen = document.querySelector('.info__read-more-button')
-var textAll = document.querySelector('.info__text')
-
-buttonTextOpen.addEventListener('click', function (evt) { 
-  evt.preventDefault()
-    if (buttonTextOpen.value == 'Скрыть') {
-      buttonTextOpen.value = 'Читать далее'
-      textAll.style.maxHeight = textHeiht + 'px'
-      document.querySelector('.info__read-more-image').style.transform = 'rotate(0)';
-    } else {
-      buttonTextOpen.value = 'Скрыть'
-      textAll.style.maxHeight = 'auto'
   
-      document.querySelector('.info__read-more-image').style.transform = 'rotate(180deg)';
-    }
+  var buttonTextOpen = document.querySelector('.info__read-more-button')
+  var textAll = document.querySelector('.info__text')
+
+  buttonTextOpen.addEventListener('click', function (evt) { 
+    evt.preventDefault()
+      if (buttonTextOpen.value == 'Скрыть') {
+        buttonTextOpen.value = 'Читать далее'
+        textAll.classList.add('info__text-js')
+        // textAll.style.maxHeight = textHeiht + 'px'
+        document.querySelector('.info__read-more-image').style.transform = 'rotate(0)'
+      } else {
+        buttonTextOpen.value = 'Скрыть'
+        textAll.classList.remove('info__text-js')
+        document.querySelector('.info__read-more-image').style.transform = 'rotate(180deg)'
+      }
+  })
+
 })
+
